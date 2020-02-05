f6548618a71a70f58bc0510ea5f839bb815dc180
Sean O'Donnell <sean@seanodonnell.com>
2020-02-04 21:46:20 -0800

    added basic 'code' command check and install instructions if missing.

--- install.extensions.sh
+++ install.extensions.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+CODE=`which code`
+
+if [ -z $CODE ]; then
+  echo -e "\nVS Code CLI Command not found.\n"
+  echo -e "Installation Instructions:\n"
+  echo -e "\tMacOS: https://code.visualstudio.com/docs/setup/mac\n"
+  echo -e "\tLinux: https://code.visualstudio.com/docs/setup/linux\n"
+  exit
+fi
+
+echo -e "Synchronizing VS Code Extensions...\n"
+
 for i in `cat extensions.txt`; do
   code --install-extension ${i}
 done
+
+echo -e "Done.\n"
