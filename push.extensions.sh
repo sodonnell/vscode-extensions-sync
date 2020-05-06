7976e1df46062695d9192ed4d452559b7974cce2
Sean O'Donnell <sean@seanodonnell.com>
2020-05-06 01:01:33 -0700

    updated push.extensions.sh

--- push.extensions.sh
+++ push.extensions.sh
@@ -2,7 +2,7 @@
 
 echo -e "Updating Github Repo...\n"
 
-DIFFS=`diff extensions.txt extensions.bak | tr '\n' ' '`
+DIFFS=`diff extensions.bak extensions.txt | tr '\n' ' '`
 
 git commit extensions.txt -m "updated extensions.txt: ${DIFFS}" .
 git push origin master
